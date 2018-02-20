3871abd7fe88678e76005b94175ea49eea2a272f
Romain niam0r <niamor@mm.st>
2018-02-20 11:55:37 +0000

    installed axios maybe replace by fetch later

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 const API_KEY = 'ba576edd191248602bc49dfeba2aca48';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},uk`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
