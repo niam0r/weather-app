cddbef894ab8eaf0830f62e32b5960bd5e3a32fc
Romain niam0r <niamor@mm.st>
2018-02-20 15:35:27 +0000

    created WeatherList

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'ba576edd191248602bc49dfeba2aca48';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -9,8 +9,6 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},uk`;
   const request = axios.get(url);
   // const request = fetch(url);
-
-  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
---
+++ src/containers/WeatherList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
