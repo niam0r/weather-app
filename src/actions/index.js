5f9e4e797260748a465c544b2d44954cd95d9843
Romain niam0r <niamor@mm.st>
2018-02-20 12:34:16 +0000

    implemented WeatherReducer, all working up to important lecture 59

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = 'ba576edd191248602bc49dfeba2aca48';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},uk`;
+  const request = axios.get(url);
+  // const request = fetch(url);
+
+  console.log('Request:', request);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
